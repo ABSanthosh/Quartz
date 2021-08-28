8ca815f01d21faff9095b0748c2e5126c4d98f6d
ABSanthosh <a.b.santhosh02@gmail.com>
2021-08-29 00:25:49 +0530

    7

---
+++ src/Components/FancyButton/FancyButton.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./FancyButton.scss";
+import { useAuth } from "../../hooks/useAuth";
+
+function FancyButton({ text, className, isLogin, children, ...props }) {
+  return (
+    <div
+      {...props}
+      className={`FancyButtonWrapper ${className}`}
+    >
+      {text}
+      {children}
+    </div>
+  );
+}
+
+FancyButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node,
+  isLogin: PropTypes.bool,
+};
+
+FancyButton.defaultProps = {
+  className: "",
+  children: null,
+  isLogin: false,
+};
+
+export default FancyButton;
