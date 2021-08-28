d83ae6376ae43a0f9b9fe18068b47f3dda1a1bcc
ABSanthosh <a.b.santhosh02@gmail.com>
2021-08-29 00:27:20 +0530

    21

---
+++ src/views/Boards/Components/BoardHeader/BoardHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./BoardHeader.scss";
+
+const BoardHeader = (props) => <div className="BoardHeaderWrapper">QUARTZ</div>;
+
+BoardHeader.propTypes = {
+  // bla: PropTypes.string,
+};
+
+BoardHeader.defaultProps = {
+  // bla: 'test',
+};
+
+export default BoardHeader;
