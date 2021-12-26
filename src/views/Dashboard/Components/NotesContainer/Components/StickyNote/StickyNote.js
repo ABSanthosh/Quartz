13f56fd9fe51934826a6e25ffe16da8fd04c042b
ABSanthosh <a.b.santhosh02@gmail.com>
2021-12-26 18:34:44 +0530

    refactor(StickyNotes): Added new features and changes structure

--- src/views/Dashboard/Components/NotesContainer/Components/StickyNote/StickyNote.js
+++ src/views/Dashboard/Components/NotesContainer/Components/StickyNote/StickyNote.js
@@ -1,31 +1,69 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import "./StickyNote.scss";
-import { ReactComponent as Dots } from "../../../../Assets/Img/StickyNotes/dots.svg";
-import { ReactComponent as Photo } from "../../../../Assets/Img/StickyNotes/photo.svg";
-import Bold from "../../../../Assets/Img/StickyNotes/Bold.png";
-import Italics from "../../../../Assets/Img/StickyNotes/Italic.png";
-import List from "../../../../Assets/Img/StickyNotes/list.png";
-import Strike from "../../../../Assets/Img/StickyNotes/strikethrough.png";
+import { ReactComponent as Dots } from "../../../../../../Assets/Img/StickyNotes/dots.svg";
+import { ReactComponent as Photo } from "../../../../../../Assets/Img/StickyNotes/photo.svg";
 
-function StickyNote({ data }) {
+import Bold from "../../../../../../Assets/Img/StickyNotes/Bold.png";
+import Italics from "../../../../../../Assets/Img/StickyNotes/Italic.png";
+import List from "../../../../../../Assets/Img/StickyNotes/list.png";
+import Strike from "../../../../../../Assets/Img/StickyNotes/strikethrough.png";
+import ContextMenu from "../ContextMenu/ContextMenu";
+
+import { useStoreState } from "easy-peasy";
+
+function StickyNote() {
   const [isBold, setIsBold] = useState(false);
   const [isItalic, setIsItalic] = useState(false);
   const [isList, setIsList] = useState(false);
   const [isStrike, setIsStrike] = useState(false);
+  const [isContext, setIsContext] = useState(false);
+
+  const data = useStoreState((state) => state.selectedNote);
+
 
   return (
     <div
       className="StickyNoteWrapper"
       style={{ backgroundColor: data.theme.primary }}
+      onContextMenu={(e) => {
+        console.log(data);
+      }}
     >
+      <ContextMenu
+        setIsContext={setIsContext}
+        data={data}
+        isContext={isContext}
+        className="StickyNoteWrapper--contextMenu"
+      />
       <header className="StickyNoteWrapper__header">
         <div
           className="StickyNoteWrapper__header--stripe"
           style={{ backgroundColor: data.theme.secondary }}
         />
         <div className="StickyNoteWrapper__header--details">
-          <Dots />
+          <Dots
+            className="StickyNoteWrapper__optionsButton"
+            onClick={() => {
+              setIsContext(!isContext);
+              const optionsButton = document.querySelector(
+                ".StickyNoteWrapper__optionsButton"
+              );
+              var position = optionsButton.getBoundingClientRect();
+              var positionX = position.x;
+              var positionY = position.y;
+
+              var contextMenu = document.querySelector(
+                ".StickyNoteWrapper--contextMenu"
+              );
+
+              contextMenu.style.left =
+                positionX -
+                (contextMenu.clientWidth - optionsButton.clientWidth) +
+                "px";
+              contextMenu.style.top = positionY + 8 + "px";
+            }}
+          />
         </div>
       </header>
       <article className="StickyNoteWrapper__content">
