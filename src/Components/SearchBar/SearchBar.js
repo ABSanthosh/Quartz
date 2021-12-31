c0c2d2d7883e228efb080e11fbb0adf658480bef
ABSanthosh <a.b.santhosh02@gmail.com>
2021-12-31 23:55:05 +0530

    feat(StickyNotes): Added New search component

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,41 @@
+import propTypes from "prop-types";
+import React, { useState } from "react";
+import "./SearchBar.scss";
+
+function SearchBar({ callBack, placeholder, ...props }) {
+  const [inputValue, setInputValue] = useState("");
+  return (
+    <div className="SearchBarWrapper" {...props}>
+      <div className="SearchBarWrapper__icon" />
+      <input
+        className="SearchBarWrapper__input"
+        placeholder={placeholder}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+          callBack(e.target.value);
+        }}
+      />
+      {inputValue.length > 0 && (
+        <div
+          className="SearchBarWrapper__clear"
+          onClick={() => {
+            setInputValue("");
+            callBack("");
+            document.querySelector(".SearchBarWrapper__input").value = "";
+            document.querySelector(".SearchBarWrapper__input").focus();
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+SearchBar.propTypes = {
+  callBack: propTypes.func.isRequired,
+};
+
+SearchBar.defaultProps = {
+  // bla: 'test',
+};
+
+export default SearchBar;
