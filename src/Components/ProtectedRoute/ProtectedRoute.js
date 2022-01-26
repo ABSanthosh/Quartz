5c3124527c7b52697ebf303fe681329c891efff3
ABSanthosh <a.b.santhosh02@gmail.com>
2022-01-26 20:49:01 +0530

    refactor(Fix): Nexted renders fixed

--- src/Components/ProtectedRoute/ProtectedRoute.js
+++ src/Components/ProtectedRoute/ProtectedRoute.js
@@ -2,16 +2,25 @@ import { useAuth } from "../../hooks/useAuth";
 import Delay from "react-delay";
 import { Redirect, Route } from "react-router-dom";
 import { useSupabaseLoading } from "../../hooks/useSupabaseLoading";
+import { useEffect } from "react";
 
 export default function ProtectedRoute(props) {
   const { userState, status } = useAuth();
   const { startFBLoading, stopFBLoading } = useSupabaseLoading();
 
+  useEffect(() => {
+    if (userState === null) {
+      startFBLoading();
+    }else{
+      stopFBLoading();
+    }
+  });
+
   if (status === "ready" && userState) {
     return <Route {...props}>{stopFBLoading()}</Route>;
   }
   if (userState !== null) {
-    return <Delay wait={250}>{startFBLoading()}</Delay>;
+    return <Delay wait={250}></Delay>;
   }
 
   return <Redirect to="/" />;
