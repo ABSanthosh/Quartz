8897a3915a88fd1df91462469a5af07854e818a8
ABSanthosh <a.b.santhosh02@gmail.com>
2022-01-26 20:52:03 +0530

    feat(Notes): Features to paste image/ drag and drop etc

--- src/views/Dashboard/Components/NotesContainer/Components/StickyNote/ContentEditor.js
+++ src/views/Dashboard/Components/NotesContainer/Components/StickyNote/ContentEditor.js
@@ -2,6 +2,8 @@ import { useStoreActions, useStoreState } from "easy-peasy";
 import propTypes from "prop-types";
 import React from "react";
 import ContentEditable from "react-contenteditable";
+import { useSupabaseLoading } from "../../../../../../hooks/useSupabaseLoading";
+import pasteHtmlAtCaret from "../../../../../../Utils/pasteHtmlAtCaret";
 
 function ContentEditor({ handleCursorElement }) {
   const data = useStoreState((state) => state.selectedNote);
@@ -9,8 +11,27 @@ function ContentEditor({ handleCursorElement }) {
     (action) => action.setSelectedNoteContent
   );
 
+  const updateSelectedNote = useStoreActions(
+    (action) => action.updateSelectedNote
+  );
+
+  const { startFBLoading, stopFBLoading } = useSupabaseLoading();
+
   return (
     <div className="StickyNoteWrapper__content--editableContainer">
+      {/* <div className="StickyNoteWrapper__content--imagePreview">
+        {data.images.map((image, index) => {
+          console.log(image);
+          return (
+            <img
+              src={image}
+              alt="uploadedImg"
+              className="StickyNoteWrapper__content--editableContainer--image"
+              key={index}
+            />
+          );
+        })}
+      </div> */}
       <ContentEditable
         className="StickyNoteWrapper__content--editableContent"
         html={data.content}
@@ -60,6 +81,84 @@ function ContentEditor({ handleCursorElement }) {
               ).innerHTML,
             });
           }
+        }}
+        onDrop={(e) => {
+          startFBLoading();
+          e.preventDefault();
+          e.stopPropagation();
+
+          if (e.dataTransfer.files.length > 0) {
+            var reader = new FileReader();
+            var baseString;
+            reader.onloadend = function () {
+              baseString = reader.result;
+
+              var newImage = document.createElement("img");
+              newImage.src = baseString;
+              newImage.alt = "";
+
+              pasteHtmlAtCaret(newImage.outerHTML);
+            };
+            reader.readAsDataURL(e.dataTransfer.files[0]);
+            updateSelectedNote({
+              id: data.id,
+              content: document.querySelector(
+                ".StickyNoteWrapper__content--editableContent"
+              ).innerHTML,
+            });
+          }
+          stopFBLoading();
+        }}
+        onDragOver={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          e.dataTransfer.dropEffect = "copy";
+        }}
+        onPaste={(e) => {
+          startFBLoading();
+          e.preventDefault();
+
+          try {
+            var reader = new FileReader();
+            var baseString;
+            reader.onloadend = function () {
+              baseString = reader.result;
+
+              var newImage = document.createElement("img");
+              newImage.src = baseString;
+              newImage.alt = "";
+
+              pasteHtmlAtCaret(newImage.outerHTML);
+            };
+            reader.readAsDataURL(e.dataTransfer.files[0]);
+            updateSelectedNote({
+              id: data.id,
+              content: document.querySelector(
+                ".StickyNoteWrapper__content--editableContent"
+              ).innerHTML,
+            });
+          } catch (err) {
+            var pastedData = e.clipboardData.getData("text/html");
+            var pastedImage = document.createElement("img");
+
+            let src = pastedData.match(/src="(.*?)"/);
+
+            if (src) {
+              pastedImage.src = src[1];
+              pastedImage.alt = "";
+
+              pasteHtmlAtCaret(pastedImage.outerHTML);
+            }else{
+              pasteHtmlAtCaret(pastedData);
+            }
+            updateSelectedNote({
+              id: data.id,
+              content: document.querySelector(
+                ".StickyNoteWrapper__content--editableContent"
+              ).innerHTML,
+            });
+          }
+          stopFBLoading();
         }}
       />
     </div>
