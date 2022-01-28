e853528d1d6b8785587ee7b356aee77f10b6cd66
ABSanthosh <a.b.santhosh02@gmail.com>
2022-01-29 02:24:05 +0530

    refactor(Loading): Unknown issue with loading screen

--- src/Components/ProtectedRoute/ProtectedRoute.js
+++ src/Components/ProtectedRoute/ProtectedRoute.js
@@ -2,19 +2,18 @@ import { useAuth } from "../../hooks/useAuth";
 import Delay from "react-delay";
 import { Redirect, Route } from "react-router-dom";
 import { useSupabaseLoading } from "../../hooks/useSupabaseLoading";
-import { useEffect } from "react";
 
 export default function ProtectedRoute(props) {
   const { userState, status } = useAuth();
-  const { startFBLoading, stopFBLoading } = useSupabaseLoading();
+  const { stopFBLoading } = useSupabaseLoading();
 
-  useEffect(() => {
-    if (userState === null) {
-      startFBLoading();
-    }else{
-      stopFBLoading();
-    }
-  });
+  // useEffect(() => {
+  //   if (userState === null) {
+  //     startFBLoading();
+  //   } else {
+  //     stopFBLoading();
+  //   }
+  // });
 
   if (status === "ready" && userState) {
     return <Route {...props}>{stopFBLoading()}</Route>;
