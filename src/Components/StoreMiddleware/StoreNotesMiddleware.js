ac4bd6c1231a4c4f7de68d6a2e9b4e6d60767d78
ABSanthosh <a.b.santhosh02@gmail.com>
2022-02-01 20:31:35 +0530

    refactor(Routing): Routing fixed for newly added notes

--- src/Components/StoreMiddleware/StoreNotesMiddleware.js
+++ src/Components/StoreMiddleware/StoreNotesMiddleware.js
@@ -14,24 +14,32 @@ function StoreNotesMiddleware(props) {
 
   useEffect(() => {
     // if url param is notes and current option is not notes, set current option to notes
-    if (currentOption !== "notes") {
-      setCurrentOption("notes");
-    }
-    // if url param for modeId is defined, set selected note to that note
-    // else the note will be set to 0th note from store so just change path to that note
-    if (props.computedMatch.params.modeId) {
-      setSelectedNote(parseInt(props.computedMatch.params.modeId));
-    } else {
-      if (selectedNote) {
+    if (props.computedMatch.params.mode === "notes") {
+      if (currentOption !== "notes") {
+        setCurrentOption("notes");
+      }
+      // if url param for modeId is defined, set selected note to that note
+      // else the note will be set to 0th note from store so just change path to that note
+      if (props.computedMatch.params.modeId) {
+        if (
+          selectedNote &&
+          selectedNote.id !== parseInt(props.computedMatch.params.modeId)
+        ) {
+          setSelectedNote(parseInt(props.computedMatch.params.modeId));
+          history.push(
+            `/app/dashboard/notes/${props.computedMatch.params.modeId}`
+          );
+        }
+      } else if (selectedNote) {
         history.push(`/app/dashboard/notes/${selectedNote.id}`);
       }
-    }
 
-    if (
-      selectedNote &&
-      props.computedMatch.params.modeId !== `${selectedNote.id}`
-    ) {
-      history.push(`/app/dashboard/notes/${selectedNote.id}`);
+      if (
+        selectedNote &&
+        props.computedMatch.params.modeId !== `${selectedNote.id}`
+      ) {
+        history.push(`/app/dashboard/notes/${selectedNote.id}`);
+      }
     }
   });
 
