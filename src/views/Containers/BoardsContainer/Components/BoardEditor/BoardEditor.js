c390785987f670aeee940688594e3f00cbf1960b
ABSanthosh <a.b.santhosh02@gmail.com>
2022-02-03 21:41:32 +0530

    feat(Board): Background image change for each note and glass panels

--- src/views/Containers/BoardsContainer/Components/BoardEditor/BoardEditor.js
+++ src/views/Containers/BoardsContainer/Components/BoardEditor/BoardEditor.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import "./BoardEditor.scss";
 import { ControlIconsDefinitions } from "../../../../../Assets/Font/IconMap";
@@ -13,6 +13,21 @@ function BoardEditor({ navState }) {
   const setSelectedBoardTitle = useStoreActions(
     (action) => action.setSelectedBoardTitle
   );
+
+  useEffect(() => {
+    document.querySelector(
+      ".DashboardWrapper__frame "
+    ).style.backgroundImage = `url(${selectedBoard.backgroundImage})`;
+    document.querySelector(".DashboardWrapper__frame ").style.backgroundSize =
+      "cover";
+
+    document.querySelector(
+      ".DashboardWrapper__frame "
+    ).style.backgroundPosition = "50%";
+
+    document.querySelector(".DashboardWrapper__frame ").style.backgroundRepeat =
+      "no-repeat";
+  });
 
   return (
     <>
@@ -79,7 +94,44 @@ function BoardEditor({ navState }) {
         </div>
         <div className="DashboardWrapper__subHeader--right"></div>
       </div>
-      <div className="BoardEditorWrapper">Test content</div>
+      <div className="BoardEditorWrapper">
+        <div className="BoardEditorWrapper__contentContainer">
+          {selectedBoard.boardPanels.map((panel, index) => {
+            return (
+              <div className="BoardEditorWrapper__panel" key={index}>
+                <div className="BoardEditorWrapper__panel--title">
+                  <div className="BoardEditorWrapper__panel--title--left">
+                    {panel.title}
+                  </div>
+                  <div className="BoardEditorWrapper__panel--title--right">
+                    <span className="controlIcons" style={{ cursor: "grab" }}>
+                      {ControlIconsDefinitions.More}
+                    </span>
+                    <span
+                      className="controlIcons"
+                      style={{ cursor: "pointer" }}
+                    >
+                      {ControlIconsDefinitions.Add}
+                    </span>
+                  </div>
+                </div>
+                <div className="BoardEditorWrapper__panel--content">
+                  {panel.panelItems.map((card, index) => {
+                    return (
+                      <div
+                        className="BoardEditorWrapper__panel--content--card"
+                        key={index}
+                      >
+                        {card.content}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
