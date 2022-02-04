1f91f0a12f614956af75d6db2d5c1d50e7ff76b2
ABSanthosh <a.b.santhosh02@gmail.com>
2022-02-04 15:23:57 +0530

    refactor(Boards): Background image for cards

--- src/views/Containers/BoardsContainer/Components/BoardsHome/BoardsHome.js
+++ src/views/Containers/BoardsContainer/Components/BoardsHome/BoardsHome.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import "./BoardsHome.scss";
-import { useStoreState } from "easy-peasy";
+import { useStoreActions, useStoreState } from "easy-peasy";
 import { useHistory } from "react-router-dom";
+import paperEffect from "../../../../../Assets/Img/paperEffect.png";
 
 function BoardsHome({ navState }) {
   const boards = useStoreState((state) => state.boards);
   let history = useHistory();
+  const setSelectedBoard = useStoreActions((action) => action.setSelectedBoard);
+
+  useEffect(() => {
+    console.log(
+      document.querySelector(".DashboardWrapper__frame ").style.backgroundImage,
+      paperEffect
+    );
+    document.querySelector(
+      ".DashboardWrapper__frame "
+    ).style.backgroundImage = `url(${paperEffect})`;
+    // document.querySelector(".DashboardWrapper__frame ").style.backgroundSize =
+    //   "cover";
+
+    // document.querySelector(
+    //   ".DashboardWrapper__frame "
+    // ).style.backgroundPosition = "50%";
+
+    // document.querySelector(".DashboardWrapper__frame ").style.backgroundRepeat =
+    //   "no-repeat";
+  });
 
   return (
     <>
@@ -28,13 +49,20 @@ function BoardsHome({ navState }) {
               className="BoardsHomeWrapper__item"
               key={index}
               onClick={() => {
+                setSelectedBoard(parseInt(board.id));
                 history.push(`/app/dashboard/boards/${board.id}`);
               }}
+              style={{
+                backgroundImage: `url(${board.backgroundImage})`,
+                backgroundSize: "cover",
+              }}
             >
-              <div className="BoardsHomeWrapper__item__title">
-                {board.title}
-                <br />
-                {board.id}
+              <div className="BoardsHomeWrapper__item--pseudo">
+                <div className="BoardsHomeWrapper__item__title">
+                  {board.title}
+                  <br />
+                  {board.id}
+                </div>
               </div>
             </div>
           ))}
