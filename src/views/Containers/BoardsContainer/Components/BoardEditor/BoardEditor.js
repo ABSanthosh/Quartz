38aa962fb384dcce7abbe7645a9198fe06d26553
ABSanthosh <a.b.santhosh02@gmail.com>
2022-02-09 12:53:45 +0530

    refactor(Boards): Option to star board from within editor

--- src/views/Containers/BoardsContainer/Components/BoardEditor/BoardEditor.js
+++ src/views/Containers/BoardsContainer/Components/BoardEditor/BoardEditor.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./BoardEditor.scss";
 import { ControlIconsDefinitions } from "../../../../../Assets/Font/IconMap";
-import { useStoreActions, useStoreState } from "easy-peasy";
+import { action, useStoreActions, useStoreState } from "easy-peasy";
 import { Link } from "react-router-dom";
 import SmallContentEditable from "../../../../../Components/SmallContentEditable/SmallContentEditable";
 
@@ -14,11 +14,14 @@ function BoardEditor({ navState }) {
     (action) => action.setSelectedBoardTitle
   );
   const addPanel = useStoreActions((action) => action.addPanel);
-
+  const setBoardStarState = useStoreActions(
+    (action) => action.setBoardStarState
+  );
   const addPanelItem = useStoreActions((action) => action.addPanelItem);
   const setBoardPanelTitle = useStoreActions(
     (action) => action.setBoardPanelTitle
   );
+  const [isHovering, setIsHovering] = useState(false);
 
   const closeStyle = {
     backgroundColor: "rgba(255, 255, 255, 0.685)",
@@ -48,6 +51,35 @@ function BoardEditor({ navState }) {
           >
             {ControlIconsDefinitions.ChevronRight}
           </span>
+          <span
+            className={`icon-lg icon-star${
+              selectedBoard.isStarred && !isHovering ? "-starred" : ""
+            } BoardsHomeWrapper__item__subContainer--star${
+              selectedBoard.isStarred ? "Active" : ""
+            }`}
+            title={
+              selectedBoard.isStarred
+                ? "Click to remove from starred list"
+                : "Click to add to starred list"
+            }
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+            onClick={() => {
+              console.log(selectedBoard.isStarred);
+
+              setBoardStarState({
+                id: parseInt(selectedBoard.id),
+                starState: !selectedBoard.isStarred,
+              });
+            }}
+            style={{
+              fontSize: "20px",
+              marginRight: "0px",
+              cursor: "pointer",
+              color: selectedBoard.isStarred ? "#f2d600" : "black",
+              opacity: "1",
+            }}
+          />
           <SmallContentEditable
             html={selectedBoard.title}
             setOptionalEllipsis={setIsEllipsisable}
@@ -157,193 +189,3 @@ BoardEditor.propTypes = {
 BoardEditor.defaultProps = {};
 
 export default BoardEditor;
-
-/*
- {selectedBoard.boardPanels.map((panel, index) => {
-            return (
-              <div className="BoardEditorWrapper__panel" key={index}>
-                <div className="BoardEditorWrapper__panel--title">
-                  <div className="BoardEditorWrapper__panel--title--left">
-                    <SmallContentEditable
-                      html={panel.title}
-                      setNewValue={(newValue) => {
-                        setBoardPanelTitle({
-                          id: panel.id,
-                          title: newValue,
-                        });
-                      }}
-                    />
-                  </div>
-                  <div className="BoardEditorWrapper__panel--title--right">
-                    <span className="controlIcons" style={{ cursor: "grab" }}>
-                      {ControlIconsDefinitions.More}
-                    </span>
-                    <span
-                      className="controlIcons"
-                      style={{ cursor: "pointer" }}
-                      onClick={() => {
-                        addPanelItem({
-                          id: panel.id,
-                        });
-                      }}
-                    >
-                      {ControlIconsDefinitions.Add}
-                    </span>
-                  </div>
-                </div>
-                <div className="BoardEditorWrapper__panel--content">
-                  {panel.panelItems.map((card, index) => {
-                    return (
-                      <div
-                        className="BoardEditorWrapper__panel--content--card"
-                        key={index}
-                      >
-                        {card.content}
-                      </div>
-                    );
-                  })}
-                  {panel.panelItems.length === 0 && (
-                    <div
-                      className="BoardEditorWrapper__panel--content--placeholderCard"
-                      onClick={() => {
-                        addPanelItem({
-                          id: panel.id,
-                        });
-                      }}
-                    >
-                      <span
-                        className="controlIcons"
-                        style={{ fontSize: "15px" }}
-                      >
-                        {ControlIconsDefinitions.Add}
-                      </span>
-                      Add Item
-                    </div>
-                  )}
-                </div>
-              </div>
-            );
-          })}
-          <div
-            className="BoardEditorWrapper__contentContainer--addPanel"
-            onClick={() => {
-              addPanel({});
-            }}
-          >
-            <span className="controlIcons">{ControlIconsDefinitions.Add}</span>
-            Add Panel
-          </div>
-*/
-
-/*
-<DragDropContext>
-            {selectedBoard.boardPanels.map((panel, index) => {
-              return (
-                <div className="BoardEditorWrapper__panel" key={index}>
-                  <div className="BoardEditorWrapper__panel--title">
-                    <div className="BoardEditorWrapper__panel--title--left">
-                      <SmallContentEditable
-                        html={panel.title}
-                        setNewValue={(newValue) => {
-                          setBoardPanelTitle({
-                            id: panel.id,
-                            title: newValue,
-                          });
-                        }}
-                      />
-                    </div>
-                    <div className="BoardEditorWrapper__panel--title--right">
-                      <span className="controlIcons" style={{ cursor: "grab" }}>
-                        {ControlIconsDefinitions.More}
-                      </span>
-                      <span
-                        className="controlIcons"
-                        style={{ cursor: "pointer" }}
-                        onClick={() => {
-                          addPanelItem({
-                            id: panel.id,
-                          });
-                        }}
-                      >
-                        {ControlIconsDefinitions.Add}
-                      </span>
-                    </div>
-                  </div>
-                  <Droppable droppableId={panel.id} key={index}>
-                    {(provided, snapshot) => (
-                      <div
-                        className="BoardEditorWrapper__panel--content"
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        style={{
-                          background: snapshot.isDraggingOver
-                            ? "lightblue"
-                            : "",
-                        }}
-                      >
-                        {panel.panelItems.map((card, index) => {
-                          return (
-                            <Draggable
-                              key={index}
-                              draggableId={card.id}
-                              index={index}
-                            >
-                              {(provided, snapshot) => {
-                                return (
-                                  <div
-                                    className="BoardEditorWrapper__panel--content--card"
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    style={{
-                                      userSelect: "none",
-                                      backgroundColor: snapshot.isDragging
-                                        ? "#263B4A"
-                                        : "",
-                                      ...provided.draggableProps.style,
-                                    }}
-                                  >
-                                    {card.content}
-                                  </div>
-                                );
-                              }}
-                            </Draggable>
-                          );
-                        })}
-                        {panel.panelItems.length === 0 && (
-                          <div
-                            className="BoardEditorWrapper__panel--content--placeholderCard"
-                            onClick={() => {
-                              addPanelItem({
-                                id: panel.id,
-                              });
-                            }}
-                          >
-                            <span
-                              className="controlIcons"
-                              style={{ fontSize: "15px" }}
-                            >
-                              {ControlIconsDefinitions.Add}
-                            </span>
-                            Add Item
-                          </div>
-                        )}
-                        {provided.placeholder}
-                      </div>
-                    )}
-                  </Droppable>
-                </div>
-              );
-            })}
-          </DragDropContext>
-
-          <div
-            className="BoardEditorWrapper__contentContainer--addPanel"
-            onClick={() => {
-              addPanel({});
-            }}
-          >
-            <span className="controlIcons">{ControlIconsDefinitions.Add}</span>
-            Add Panel
-          </div>
-*/
