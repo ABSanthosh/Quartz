488df4d87d1144125e7232aec2d0b71da577caa4
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-08 00:10:14 +0530

    feat(PWA): Complete PWA setup

--- app/root.jsx
+++ app/root.jsx
@@ -5,36 +5,71 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLocation,
+  useMatches,
 } from "@remix-run/react";
-
+import React from "react";
 import resetStyle from "./styles/reset.css";
 import globalStyle from "./styles/global.css";
-
 export function links() {
   return [
     { rel: "stylesheet", href: resetStyle },
     { rel: "stylesheet", href: globalStyle },
   ];
 }
-
 export const meta = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
-
 export default function App() {
+  let location = useLocation();
+  let matches = useMatches();
+
+  let isMount = true;
+  React.useEffect(() => {
+    let mounted = isMount;
+    isMount = false;
+    if ("serviceWorker" in navigator) {
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller?.postMessage({
+          type: "REMIX_NAVIGATION",
+          isMount: mounted,
+          location,
+          matches,
+          manifest: window.__remixManifest,
+        });
+      } else {
+        let listener = async () => {
+          await navigator.serviceWorker.ready;
+          navigator.serviceWorker.controller?.postMessage({
+            type: "REMIX_NAVIGATION",
+            isMount: mounted,
+            location,
+            matches,
+            manifest: window.__remixManifest,
+          });
+        };
+        navigator.serviceWorker.addEventListener("controllerchange", listener);
+        return () => {
+          navigator.serviceWorker.removeEventListener(
+            "controllerchange",
+            listener
+          );
+        };
+      }
+    }
+  }, [location]);
+
   return (
     <html lang="en">
       <head>
         <Meta />
+        <link rel="manifest" href="/resources/manifest.json" />
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <Outlet /> <ScrollRestoration /> <Scripts /> <LiveReload />
       </body>
     </html>
   );
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remixd</h1>
     </div>
   );
 }
