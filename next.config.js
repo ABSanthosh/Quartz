1f093cf493275ae6a7bacb2b0818998afb85498c
ABSanthosh <a.b.santhosh02@gmail.com>
2023-07-05 21:40:08 +0530

    feat: project setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require("path");
 
-module.exports = nextConfig
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.forEach((rule) => {
+      const { oneOf } = rule;
+      if (oneOf) {
+        oneOf.forEach((one) => {
+          if (!`${one.issuer?.and}`.includes("_app")) return;
+          one.issuer.and = [path.resolve(__dirname)];
+        });
+      }
+    });
+    return config;
+  },
+  sassOptions: {
+    additionalData: `@import "src/styles/root/_mixins.scss";`,
+  },
+};
+
+module.exports = nextConfig;
